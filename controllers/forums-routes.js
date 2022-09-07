66e77bfcce95b2725e876d3b0d91d2a00906c593
AHewetson <a.hewetson@hotmail.com>
2022-09-06 21:47:38 -0400

    Update forums-routes.js

--- controllers/forums-routes.js
+++ controllers/forums-routes.js
@@ -36,41 +36,29 @@ router.get("/", withAuth, (req, res) => {
         res.status(500).json(err);
     });
 });
-//testing area
-router.get("/forums-baseball", withAuth, (req, res) => {
-  Post.findAll({
-    where: {
-        user_id: req.session.user_id
-    },
-    attributes: ["id", "post_content", "title", "created_at",
-    [sequelize.literal('(SELECT COUNT(*) FROM love WHERE post.id = love.post_id)'), 'love_count'],
-    [sequelize.literal('(SELECT COUNT(*) FROM dislike WHERE post.id = dislike.post_id)'), 'dislike_count']
-  ],
-    include: [
-        {
-            model: Comment, 
-            attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
-            include: {
-                model: User, 
-                attributes: ["username"]
-            }
-        },
-        {
-            model: User, 
-            attributes: ["username"]
-        }
-    ]
-})
-.then(dbPostData => {
-    const posts = dbPostData.map(post => post.get({ plain: true }));
-    res.render("forums-baseball", { posts, loggedIn: true });
-})
-.catch(err => {
-    console.log(err);
-    res.status(500).json(err);
+//test 3
+router.get("/forums-baseball", (req, res) => {
+  res.render("forums-baseball", { posts, loggedIn: true });
 });
+router.get("/forums-basketball", (req, res) => {
+  res.render("forums-basketball", { posts, loggedIn: true });
 });
-//testing area
+router.get("/forums-hockey", (req, res) => {
+  res.render("forums-hockey", { posts, loggedIn: true });
+});
+router.get("/forums-football", (req, res) => {
+  res.render("forums-football", { posts, loggedIn: true });
+});
+router.get("/forums-fantasy", (req, res) => {
+  res.render("forums-fantasy", { posts, loggedIn: true });
+});
+router.get("/forums-fantasy-baseball", (req, res) => {
+  res.render("forums-fantasy-baseball", { posts, loggedIn: true });
+});
+router.get("/forums-fantasy-football", (req, res) => {
+  res.render("forums-fantasy-football", { posts, loggedIn: true });
+});
+//test 3
 router.get("/edit/:id", withAuth, (req, res) => {
     Post.findByPk(req.params.id, {
       attributes: [
